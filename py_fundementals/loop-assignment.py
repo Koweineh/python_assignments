c95abff3e547c3c0b356da10d77765e279225351
KOweineh <karam.owaineh@gmail.com>
2023-10-22 18:06:20 +0300

    hello world

---
+++ py_fundementals/loop-assignment.py
@@ -0,0 +1,29 @@
+# for i in range(151):
+#   print(i)
+  
+# for x in range(1,1000):
+#     print(x *5)
+
+# for i in range(1,101): 
+#     if i % 10 == 0:
+#         print("codingDojo")
+#     elif i % 5 == 0 :
+#         print("coding")
+#     else :
+#         print(i)    
+        
+# sum = 0
+# for i in range(0,500000):
+#     sum +=i
+# print(sum)
+
+# for x in range(2018,-1,-4):
+#     print(x)
+    
+low = 0
+high = 100
+mult = 10
+
+for y in range(low, high+1):
+    if y % mult == 0:
+        print(y)    
