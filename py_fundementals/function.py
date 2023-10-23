dad1e971bb599b84c2e7450ee347fa000e953256
KOweineh <karam.owaineh@gmail.com>
2023-10-23 13:50:43 +0300

    functions

---
+++ py_fundementals/function.py
@@ -0,0 +1,139 @@
+# #1
+# def a():
+#     return 5
+# print(a())
+
+# # answer is :5
+
+
+# #2
+# def a():
+#     return 5
+# print(a()+a())
+
+# # answer is :10
+
+# #3
+# def a():
+#     return 5
+#     return 10
+# print(a())
+
+# # answer is : 5
+
+# #4
+# def a():
+#     return 5
+#     print(10)
+# print(a())
+# answer is : 5
+
+# #5
+# def a():
+#     print(5)
+# x = a()
+# print(x)
+# # answer is : 5 & null
+
+# #6
+# def a(b,c):
+#     print(b+c)
+# print(a(1,2) + a(2,3))
+# # answer is : error
+
+# #7
+# def a(b,c):
+#     return str(b)+str(c)
+# print(a(2,5))
+# # answer is :25 
+
+
+# #8
+# def a():
+#     b = 100
+#     print(b)
+#     if b < 10:
+#         return 5
+#     else:
+#         return 10
+#     return 7
+# print(a())
+# # answer error
+
+# #9
+# def a(b,c):
+#     if b<c:
+#         return 7
+#     else:
+#         return 14
+#     return 3
+# print(a(2,3))
+# print(a(5,3))
+# print(a(2,3) + a(5,3))
+# #answer  7,14,21
+
+# #10
+# def a(b,c):
+#     return b+c
+#     return 10
+# print(a(3,5))
+# #answer is 8
+
+# #11
+# b = 500
+# print(b)
+# def a():
+#     b = 300
+#     print(b)
+# print(b)
+# a()
+# print(b)
+# # answer is 500 ,500,300,500
+
+#12
+# b = 500
+# print(b)
+# def a():
+#     b = 300
+#     print(b)
+#     return b
+# print(b)
+# a()
+# print(b)
+#  # answer is 500 ,500,300,500
+
+
+#13
+# b = 500
+# print(b)
+# def a():
+#     b = 300
+#     print(b)
+#     return b
+# print(b)
+# b=a()
+# print(b)
+# # 500,500,300,300
+
+#14 xxxxx
+# def a():
+#     print(1)
+#     b()
+#     print(2)
+# def b():
+#     print(3)
+# a()
+#1,3,2
+#15
+def a():
+    print(1)
+    x = b()
+    print(x)
+    return 10
+def b():
+    print(3)
+    return 5
+y = a()
+print(y)
+# 1.3.5.10
+
